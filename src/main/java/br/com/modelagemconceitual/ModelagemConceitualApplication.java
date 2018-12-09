d5957329d9ecca06fff750da00046104a4dd9bc3
Daniel Dias <daniel160598@hotmail.com>
2018-12-09 14:50:57 -0200

    Produto e associação muitos para muitos

--- src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
+++ src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
@@ -8,13 +8,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.modelagemconceitual.DAO.CategoriaDAO;
+import br.com.modelagemconceitual.DAO.ProdutoDAO;
 import br.com.modelagemconceitual.domain.Categoria;
+import br.com.modelagemconceitual.domain.Produto;
 
 @SpringBootApplication
 public class ModelagemConceitualApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaDAO categoriaDAO;
+	@Autowired
+	private ProdutoDAO produtoDAO;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -24,10 +28,22 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
-		
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
+		produtoDAO.saveAll(Arrays.asList(p1, p2, p3));
 		
 	}
 }
