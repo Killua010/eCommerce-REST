a5d1867bf6d9ade9becdf856b687a0111e4f6b25
Daniel Dias <daniel160598@hotmail.com>
2018-12-09 18:26:59 -0200

    ClientE, TipoCliente, telefones e enderecos

---
+++ src/main/java/br/com/modelagemconceitual/DAO/EnderecoDAO.java
@@ -0,0 +1,11 @@
+package br.com.modelagemconceitual.DAO;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.modelagemconceitual.domain.Endereco;
+
+@Repository
+public interface EnderecoDAO extends JpaRepository<Endereco, Integer>{
+
+}
--- src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
+++ src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
@@ -9,12 +9,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.modelagemconceitual.DAO.CategoriaDAO;
 import br.com.modelagemconceitual.DAO.CidadeDAO;
+import br.com.modelagemconceitual.DAO.ClienteDAO;
+import br.com.modelagemconceitual.DAO.EnderecoDAO;
 import br.com.modelagemconceitual.DAO.EstadoDAO;
 import br.com.modelagemconceitual.DAO.ProdutoDAO;
 import br.com.modelagemconceitual.domain.Categoria;
 import br.com.modelagemconceitual.domain.Cidade;
+import br.com.modelagemconceitual.domain.Cliente;
+import br.com.modelagemconceitual.domain.Endereco;
 import br.com.modelagemconceitual.domain.Estado;
 import br.com.modelagemconceitual.domain.Produto;
+import br.com.modelagemconceitual.domain.enums.TipoCliente;
 
 @SpringBootApplication
 public class ModelagemConceitualApplication implements CommandLineRunner{
@@ -27,6 +32,10 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 	private EstadoDAO estadoDAO;
 	@Autowired
 	private CidadeDAO cidadeDAO;
+	@Autowired
+	private ClienteDAO clienteDAO;
+	@Autowired
+	private EnderecoDAO enderecoDAO;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -56,6 +65,15 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
 		
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323","93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));	
+		
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
@@ -64,6 +82,9 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 		
 		estadoDAO.saveAll(Arrays.asList(est1, est2));
 		cidadeDAO.saveAll(Arrays.asList(c1, c2, c3));
+		
+		clienteDAO.saveAll(Arrays.asList(cli1));
+		enderecoDAO.saveAll(Arrays.asList(e1, e2));
 		
 	}
 }
