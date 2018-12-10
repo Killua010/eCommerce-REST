96eb5d80236b53f1a6f2a151d709bfe42ad9636b
Daniel Dias <daniel160598@hotmail.com>
2018-12-09 22:37:37 -0200

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
+++ src/main/java/br/com/modelagemconceitual/ModelagemConceitualApplication.java
@@ -1,5 +1,6 @@
 package br.com.modelagemconceitual;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import br.com.modelagemconceitual.DAO.CidadeDAO;
 import br.com.modelagemconceitual.DAO.ClienteDAO;
 import br.com.modelagemconceitual.DAO.EnderecoDAO;
 import br.com.modelagemconceitual.DAO.EstadoDAO;
+import br.com.modelagemconceitual.DAO.PagamentoDAO;
+import br.com.modelagemconceitual.DAO.PedidoDAO;
 import br.com.modelagemconceitual.DAO.ProdutoDAO;
 import br.com.modelagemconceitual.domain.Categoria;
 import br.com.modelagemconceitual.domain.Cidade;
 import br.com.modelagemconceitual.domain.Cliente;
 import br.com.modelagemconceitual.domain.Endereco;
 import br.com.modelagemconceitual.domain.Estado;
+import br.com.modelagemconceitual.domain.Pagamento;
+import br.com.modelagemconceitual.domain.PagamentoComBoleto;
+import br.com.modelagemconceitual.domain.PagamentoComCartao;
+import br.com.modelagemconceitual.domain.Pedido;
 import br.com.modelagemconceitual.domain.Produto;
+import br.com.modelagemconceitual.domain.enums.EstadoPagamento;
 import br.com.modelagemconceitual.domain.enums.TipoCliente;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 	private ClienteDAO clienteDAO;
 	@Autowired
 	private EnderecoDAO enderecoDAO;
+	@Autowired
+	private PedidoDAO pedidoDAO;
+	@Autowired
+	private PagamentoDAO pagamentoDAO;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ModelagemConceitualApplication.class, args);
@@ -77,6 +89,19 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
+		
 		categoriaDAO.saveAll(Arrays.asList(cat1, cat2));
 		produtoDAO.saveAll(Arrays.asList(p1, p2, p3));
 		
@@ -85,6 +110,9 @@ public class ModelagemConceitualApplication implements CommandLineRunner{
 		
 		clienteDAO.saveAll(Arrays.asList(cli1));
 		enderecoDAO.saveAll(Arrays.asList(e1, e2));
+		
+		pedidoDAO.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoDAO.saveAll(Arrays.asList(pagto1, pagto2));
 		
 	}
 }
