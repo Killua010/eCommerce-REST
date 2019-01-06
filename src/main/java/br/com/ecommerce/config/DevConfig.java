167b3160cb0d2fd6f0aba12f00264356659548cb
Daniel Dias <daniel160598@hotmail.com>
2019-01-06 19:05:26 -0200

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/br/com/ecommerce/config/DevConfig.java
+++ src/main/java/br/com/ecommerce/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import br.com.ecommerce.services.DBService;
+import br.com.ecommerce.services.EmailService;
+import br.com.ecommerce.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 	
 }
